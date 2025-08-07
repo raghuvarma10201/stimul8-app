addb7c491cb3a89b288a2a1ec1bc5e1dea6a69c4
nareshrp <naresh@rpwebapps.com>
2025-08-07 12:46:22 +0530

    new Changs Updated
    new Changs Updated

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -9,6 +9,48 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class HomePage {
 
+  menuItems = [
+    {
+      label: 'Channels',
+      route: '/channel',
+      icon: 'assets/images/icons/channels.gif'
+     
+    },
+    {
+      label: 'Templates',
+      route: '/templates',
+      icon: 'assets/images/icons/templates.gif'
+     
+    },
+    {
+      label: 'Runbooks',
+      route: '/runbooks',
+      icon: 'assets/images/icons/runbook.gif'
+     
+    },
+    {
+      label: 'Agents',
+      route: '/agents',
+      icon: 'assets/images/icons/agents.gif'
+      
+    },
+    {
+      label: 'Tools',
+      route: '/tools',
+      icon: 'assets/images/icons/tools.gif'
+    },
+    {
+      label: 'Vendors',
+      route: '/vendors',
+      icon: 'assets/images/icons/vendors.gif'
+    },
+    {
+      label: 'My Profile',
+      route: '/profile',
+      icon: 'assets/images/icons/myprofile.gif'
+    }
+  ];
+  
 
   constructor(private alertController: AlertController,private authService: AuthService) {
     
